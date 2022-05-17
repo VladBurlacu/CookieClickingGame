3a90ee63f12d0bacd91f4f560bf8c8580121c2b3
Vlad Burlacu <vladburlacu1337@gmail.com>
2022-05-17 14:21:30 +0200

    added save/load game functions using cookies but for some reason it doesn't work, will have to come back to this later

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 let score = 0;
-let cursorCost =15;
+let cursorCost = 15;
 let cursors = 0;
 let grandmaCost = 100;
 let grandmas = 0;
@@ -15,14 +15,16 @@ let ovens = 0;
         function addToScore() {
             score += 1;
             document.getElementById(`score`).innerHTML = score;
-        } addToScore(clickingPower)
+        }
+
+        addToScore(clickingPower)
     })
 
     document.getElementById(`cursorShop`).addEventListener(`click`, () => {
-        function buyCursor () {
+        function buyCursor() {
             if (score >= cursorCost) {
                 score = score - cursorCost;
-                cursors = cursors +1;
+                cursors = cursors + 1;
                 cursorCost = Math.round(cursorCost * 1.15);
 
                 document.getElementById(`score`).innerHTML = score;
@@ -30,7 +32,9 @@ let ovens = 0;
                 document.getElementById(`cursors`).innerHTML = cursors;
                 updateCookiesPerSecond();
             }
-        } buyCursor();
+        }
+
+        buyCursor();
 
         document.getElementById(`grannyShop`).addEventListener(`click`, () => {
             function buyGrandma() {
@@ -44,7 +48,9 @@ let ovens = 0;
                     document.getElementById(`grandmas`).innerHTML = grandmas;
                     updateCookiesPerSecond();
                 }
-            } buyGrandma();
+            }
+
+            buyGrandma();
 
         })
 
@@ -60,13 +66,50 @@ let ovens = 0;
                     document.getElementById(`ovens`).innerHTML = ovens;
                     updateCookiesPerSecond();
                 }
-            } buyOven();
+            }
+
+            buyOven();
 
         })
 
         function updateCookiesPerSecond() {
-            cookiesPerSecond = cursors + grandmas *5 + ovens * 70;
+            cookiesPerSecond = cursors + grandmas * 5 + ovens * 70;
             document.getElementById(`cookiespersecond`).innerHTML = cookiesPerSecond;
+        }
+
+        function loadGame() {
+            let savedGame = JSON.parse(localStorage.getItem("gameSave"));
+            if (typeof savedGame.score !== "undefined") score = savedGame.score;
+            if (typeof savedGame.clickingPower !== "undefined") clickingPower = savedGame.clickingPower;
+            if (typeof savedGame.cursorCost !== "undefined") cursorCost = savedGame.cursorCost;
+            if (typeof savedGame.cursors !== "undefined") cursors = savedGame.cursors;
+            if (typeof savedGame.grandmaCost !== "undefined") grandmaCost = savedGame.grandmaCost;
+            if (typeof savedGame.grandmas !== "undefined") grandmas = savedGame.grandmas;
+            if (typeof savedGame.ovenCost !== "undefined") ovenCost = savedGame.ovenCost;
+            if (typeof savedGame.ovens !== "undefined") ovens = savedGame.ovens;
+        }
+
+        document.getElementById(`saveGame`).addEventListener(`click`, () => {
+            function saveGame() {
+                let gameSave = {
+                    score: score,
+                    clickingPower: clickingPower,
+                    cursorCost: cursorCost,
+                    cursors: cursors,
+                    grandmaCost: grandmaCost,
+                    grandmas: grandmas,
+                    ovenCost: ovenCost,
+                    ovens: ovens
+                };
+                localStorage.setItem("gameSave", JSON.stringify(gameSave));
+            } setInterval(function () {
+                saveGame();
+            }, 30000);//30000ms = 30 seconds
+        })
+
+        window.unload = function () {
+            loadGame();
+            updateCookiesPerSecond();
         }
 
         setInterval(function () {
@@ -76,7 +119,8 @@ let ovens = 0;
             document.getElementById(`score`).innerHTML = score;
 
             document.title = score + "cookies - Cookie Clicker";
-            }, 1000) // 1000 ms = 1 second
+        }, 1000) // 1000 ms = 1 second
+
     })
 
 
